c6e6387b54602cc7601c5f72a842bc06dd648dc8
codevcn <codevoicainay@gmail.com>
2025-12-19 11:58:57 +0700

    add header application/json

--- src/services/api/voucher.api.ts
+++ src/services/api/voucher.api.ts
@@ -2,4 +2,8 @@ import { apiClient } from './api-client'
 import { TApiResponseBody, TCheckVoucherReq, TCheckVoucherRes } from '@/utils/types/api'
 
 export const postCheckVoucher = (data: TCheckVoucherReq) =>
-  apiClient.post<TApiResponseBody<TCheckVoucherRes>, TCheckVoucherReq>('/vouchers/check', data)
+  apiClient.post<TApiResponseBody<TCheckVoucherRes>, TCheckVoucherReq>('/vouchers/check', data, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
